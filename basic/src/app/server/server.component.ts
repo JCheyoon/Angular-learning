4fb37802036eae5aab0b44015e49d08a95e68914
Cheyoon Jung <jessietwo94@gmail.com>
2023-03-01 16:20:55 +0100

    learning basic

--- basic/src/app/server/server.component.ts
+++ basic/src/app/server/server.component.ts
@@ -2,14 +2,32 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-server',
-  template: `<p>Server with ID {{ serverID }} is {{ serverStatus }}</p>
-    <button class="btn btn-primary" [disabled]="!allowNewServer">
+  template: ` <h1>ServerName {{ serverName }}</h1>
+    <!--    <input-->
+    <!--      type="text"-->
+    <!--      class="form-control"-->
+    <!--      (input)="onUpdateServerName($event)"-->
+    <!--    />-->
+    <input type="text" class="form-control" [(ngModel)]="serverName" />
+
+    <button
+      class="btn btn-primary"
+      [disabled]="!allowNewServer"
+      (click)="onCreateServer()"
+    >
       Add Server
-    </button>`,
+    </button>
+    {{ serverCreationStatus }}
+
+    <p>Server with ID {{ serverID }} is {{ serverStatus }}</p>`,
+
   styles: [
     `
       p {
         color: red;
+      }
+      button {
+        margin: 5px 0;
       }
     `,
   ],
@@ -19,9 +37,17 @@ export class ServerComponent {
   serverStatus: string = 'online';
 
   allowNewServer = false;
+  serverCreationStatus = 'No server';
+  serverName: string = 'test';
   constructor() {
     setTimeout(() => {
       this.allowNewServer = true;
     }, 2000);
+  }
+  onCreateServer() {
+    this.serverCreationStatus = 'server was created Name is' + this.serverName;
+  }
+  onUpdateServerName(event: any) {
+    this.serverName = event.target.value;
   }
 }
